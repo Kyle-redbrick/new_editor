9c783c984948b59af2149c506f810f829981feb4
Kyle-redbrick <kyle@redbrick.space>
2024-01-11 16:19:01 +0900

    Add: Initial setting

---
+++ src/Page/MakersEditor/Container.js
@@ -0,0 +1,6 @@
+import React from "react";
+import View from "./View";
+
+export default function Container() {
+  return <View />;
+}
---
+++ src/Page/MakersEditor/View.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.scss";
+
+function View() {
+  return (
+    <div className="makersEditor">
+      <section>SNB</section>
+      <section>CONTENT</section>
+    </div>
+  );
+}
+export default View;
